a29c5f461a02d5911df47e6d9a1ba7f7be2863a8
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2020-06-16 12:56:01 -0400

    add web client to docker compose

--- nmdc_server/app.py
+++ nmdc_server/app.py
@@ -8,6 +8,6 @@ from . import __version__, api, errors
 def create_app(env: typing.Mapping[str, str]) -> FastAPI:
     app = FastAPI(title="NMDC Dataset API", version=__version__,)
     errors.attach_error_handlers(app)
-    app.include_router(api.router)
+    app.include_router(api.router, prefix="/api")
 
     return app
--- web/src/data/api.js
+++ web/src/data/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = '/api';
 
 class DataAPI {
   constructor() {
