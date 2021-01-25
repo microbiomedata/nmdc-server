f56eeca33cc9effc067f2f4f422c62a27068e9f7
Jonathan Beezley <jonathan.beezley@kitware.com>
2021-01-25 14:23:59 -0500

    Add a function to truncate all tables

--- nmdc_server/database.py
+++ nmdc_server/database.py
@@ -102,6 +102,18 @@ returns boolean as $$
     end;
 $$
 language plpgsql;
+
+CREATE OR REPLACE FUNCTION truncate_tables() RETURNS void AS $$
+DECLARE
+    statements CURSOR FOR
+        SELECT tablename FROM pg_tables
+        WHERE schemaname = 'public' and tablename <> 'alembic_version';
+BEGIN
+    FOR stmt IN statements LOOP
+        EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' CASCADE;';
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
 """
     ),
 )
