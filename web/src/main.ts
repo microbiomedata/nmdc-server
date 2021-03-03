81d5dc1f91cb49f1ad61ac2491f6bb959a0d13d2
Brandon Davis <brandon.davis@kitware.com>
2021-03-03 12:40:45 -0500

    Add sentry and google analytics (#323)

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import CompositionPlugin from '@vue/composition-api';
+import VueAnalytics from 'vue-analytics';
+import { init as SentryInit } from '@sentry/vue';
+import { Integrations } from '@sentry/tracing';
 import { sync } from 'vuex-router-sync';
 import AsyncComputed from 'vue-async-computed';
 
@@ -12,6 +15,21 @@ import App from './App.vue';
 
 Vue.use(AsyncComputed);
 Vue.use(CompositionPlugin);
+
+/**
+ * Enable instrumentation in production
+ */
+if (process.env.NODE_ENV === 'production') {
+  Vue.use(VueAnalytics, {
+    id: 'UA-68089198-4',
+  });
+  SentryInit({
+    Vue,
+    dsn: 'https://87132695029c4406afe033fb3b13b115@o267860.ingest.sentry.io/5658761',
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+}
 
 Vue.config.productionTip = false;
 
