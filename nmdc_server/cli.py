17d48e8a5633ffb6782d6f645c68461cfc06c3a0
Brandon Davis <git@subdavis.com>
2021-08-05 11:31:47 -0400

    Ability to limit ingest

--- nmdc_server/cli.py
+++ nmdc_server/cli.py
@@ -1,6 +1,8 @@
 import logging
+from typing import List
 
 import click
+from sqlalchemy.sql import text as sa_text
 
 from nmdc_server import jobs
 from nmdc_server.config import Settings
@@ -27,33 +29,40 @@ def migrate(obj):
 
 
 @cli.command()
-def truncate():
+@click.argument("tables", type=click.STRING, nargs=-1, default=None)
+def truncate(tables: List[str]):
     """Remove all existing data."""
     with create_session() as db:
-        try:
-            db.execute("select truncate_tables()").all()
-            db.commit()
-        except Exception:
-            db.rollback()
-            db.execute(
+        if len(tables) > 0:
+            for table in tables:
+                db.execute(sa_text(f"TRUNCATE TABLE {table}"))
+                db.commit()
+        else:
+            try:
+                db.execute("select truncate_tables()").all()
+                db.commit()
+            except Exception:
+                db.rollback()
+                db.execute(
+                    """
+                    DO $$ DECLARE
+                        r RECORD;
+                    BEGIN
+                        FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema())
+                        LOOP
+                            EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+                        END LOOP;
+                    END $$;
                 """
-                DO $$ DECLARE
-                     r RECORD;
-                 BEGIN
-                     FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema())
-                     LOOP
-                         EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
-                     END LOOP;
-                 END $$;
-            """
-            )
-            db.commit()
+                )
+                db.commit()
 
 
 @cli.command()
 @click.option("-v", "--verbose", count=True)
 @click.option("--function-limit", type=click.INT, default=100)
-def ingest(verbose, function_limit):
+@click.argument("tables", type=click.STRING, nargs=-1, default=None)
+def ingest(verbose, function_limit, tables):
     """Ingest the latest data from mongo."""
     level = logging.WARN
     if verbose == 1:
@@ -65,7 +74,7 @@ def ingest(verbose, function_limit):
     logger.setLevel(logging.INFO)
 
     with create_session() as db:
-        load(db, function_limit=function_limit)
+        load(db, function_limit=function_limit, tables=tables)
 
     for m, s in errors.missing.items():
         click.echo(f"missing {m}:")
