a9e67b8e4eff0d3ac4d19e0eaabda395a25a81df
Brandon Davis <git@subdavis.com>
2021-08-06 11:41:48 -0400

    lines too long

--- nmdc_server/cli.py
+++ nmdc_server/cli.py
@@ -45,14 +45,14 @@ def truncate(tables: List[str]):
                 db.rollback()
                 db.execute(
                     """
-                    DO $$ DECLARE
-                        r RECORD;
-                    BEGIN
-                        FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema())
-                        LOOP
-                            EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
-                        END LOOP;
-                    END $$;
+                DO $$ DECLARE
+                    r RECORD;
+                BEGIN
+                    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema())
+                    LOOP
+                        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+                    END LOOP;
+                END $$;
                 """
                 )
                 db.commit()
