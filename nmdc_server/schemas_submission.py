667b7e40e682b02bf4924284fd77a32287b30862
Brandon Davis <brandon.davis@kitware.com>
2022-06-16 14:33:36 -0400

    Add pydantic schemas for metadata submission

---
+++ nmdc_server/schemas_submission.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+from typing import Any, Dict, List, Optional
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class Contributor(BaseModel):
+    name: str
+    orcid: str
+    roles: List[str]
+
+
+class StudyForm(BaseModel):
+    studyName: str
+    piName: str
+    piEmail: str
+    piOrcid: str
+    linkOutWebpage: List[str]
+    studyDate: Optional[str]
+    description: str
+    notes: str
+    contributors: List[Contributor]
+
+
+class MultiOmicsForm(BaseModel):
+    datasetDoi: str
+    alternativeNames: List[str]
+    studyNumber: str
+    GOLDStudyId: str
+    JGIStudyId: str
+    NCBIBioProjectName: str
+    NCBIBioProjectId: str
+    omicsProcessingTypes: List[str]
+
+
+class MetadataSubmissionRecord(BaseModel):
+    packageName: str
+    template: str
+    studyForm: StudyForm
+    multiOmicsForm: MultiOmicsForm
+    sampleData: List[List[Any]]
+
+
+class SubmissionMetadataSchemaCreate(BaseModel):
+    metadata_submission: MetadataSubmissionRecord
+    status: Optional[str]
+
+
+class SubmissionMetadataSchema(SubmissionMetadataSchemaCreate):
+    id: UUID
+    author_orcid: str
+    created: datetime
+    status: str
+
+    class Config:
+        orm_mode = True
+
+
+SubmissionMetadataSchema.update_forward_refs()
