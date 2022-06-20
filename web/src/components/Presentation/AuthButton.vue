1f4a2779f842c2d390c2d10336a0770d2c937ec9
DeepikaGhodki <deepikaghodki@gmail.com>
2022-06-20 10:51:33 -0400

    Added click to copy component

--- web/src/components/Presentation/AuthButton.vue
+++ web/src/components/Presentation/AuthButton.vue
@@ -1,11 +1,19 @@
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent, ref } from '@vue/composition-api';
 import { stateRefs } from '@/store';
 
 export default defineComponent({
   setup() {
+    const showToast = ref(false);
+    async function copyToClipboard(token : string) {
+      await navigator.clipboard.writeText(token);
+      showToast.value = true;
+    }
     return {
       me: stateRefs.user,
+      showToast,
+      token: '123', // todo- get the actual token
+      copyToClipboard,
     };
   },
 });
@@ -14,15 +22,46 @@ export default defineComponent({
 <template>
   <div>
     <template v-if="me">
-      <v-btn
-        text
-        color="grey darken-2"
+      <v-menu
+        :close-on-content-click="false"
+        :nudge-width="100"
+        left
+        offset-x
       >
-        <v-icon left>
-          mdi-account-circle
-        </v-icon>
-        {{ me }}
-      </v-btn>
+        <template #activator="{ on, attrs }">
+          <v-btn
+            text
+            color="grey darken-2"
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon left>
+              mdi-account-circle
+            </v-icon>
+            {{ me }}
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-list>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>NMDC API Token</v-list-item-title>
+                <v-list-item-subtitle><a href="https://github.com/microbiomedata/nmdc-server/wiki/Search-API-Docs">Know more</a></v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                <v-btn
+                  icon
+                  @click="copyToClipboard(token)"
+                >
+                  <v-icon>mdi-clipboard-text-multiple-outline</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
       <v-btn
         icon
         color="grey darken-2"
@@ -30,6 +69,16 @@ export default defineComponent({
       >
         <v-icon>mdi-logout</v-icon>
       </v-btn>
+      <v-snackbar
+        v-model="showToast"
+        :timeout="1000"
+        :value="true"
+        absolute
+        bottom
+        color="primary"
+      >
+        Copied!
+      </v-snackbar>
     </template>
     <template v-else>
       <v-btn
