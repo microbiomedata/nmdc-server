43364fece1a20a286fb9e50c9a71e603ab96a956
Deepika Ghodki <deepikaghodki@gmail.com>
2022-07-05 13:51:56 -0400

    User table (#733)
    * Get all users route
    
    * Removed profile_url
    
    * Get and update user
    
    * Added a new page for listing users
    
    * Null check during user update
    
    * Update user from client
    
    * Added pagination to getUsers
    
    * UserResponse class
    
    * Removed logs
    
    * Corrected prop

--- nmdc_server/query.py
+++ nmdc_server/query.py
@@ -858,6 +858,10 @@ class MetadataSubmissionResponse(BaseSearchResponse):
     results: List[schemas_submission.SubmissionMetadataSchema]
 
 
+class UserResponse(BaseSearchResponse):
+    results: List[schemas.User]
+
+
 workflow_search_classes = [
     ReadsQCQuerySchema,
     MetagenomeAssemblyQuerySchema,
