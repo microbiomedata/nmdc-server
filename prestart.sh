5a551dcbb60b8d126959971a1618370a10e8ac22
Matt Covalt <matt@kiwiziti.com>
2022-08-15 14:34:05 -0400

    Run migrations on container entry

--- prestart.sh
+++ prestart.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
+
 export PGUSER PGPASSWORD PGDATABASE PGHOST POSTGRES_URI
 
 while ! PGDATABASE=postgres psql --quiet -c "select * from user" ; do
@@ -7,7 +9,23 @@ done
 
 echo 'Creating and migrating database'
 # in spin the database already exists
-PGDATABASE=postgres psql -c "create database nmdc_a;" || true
-PGDATABASE=postgres psql -c "create database nmdc_b;" || true
+PGDATABASE=postgres psql -c "create database nmdc_a;" \
+|| pg_dump \
+    --table "user_logins" \
+    --table "submission_metadata" \
+    --table "file_download" \
+    --file "nmdc_a-$(date +%s)-$(openssl rand -hex 32).sql" \
+    --data-only \
+    nmdc_a
+
+
+PGDATABASE=postgres psql -c "create database nmdc_b;" \
+|| pg_dump \
+    --table "user_logins" \
+    --table "submission_metadata" \
+    --table "file_download" \
+    --file "nmdc_b-$(date +%s)-$(openssl rand -hex 32).sql" \
+    --data-only \
+    nmdc_b
 
 nmdc-server migrate
