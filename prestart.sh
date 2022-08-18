f79254c11eb7dc6391c63fd7f23e5d9c794eb517
Matt Covalt <matt@kiwiziti.com>
2022-08-18 15:05:21 -0400

    Manually create database dumps

--- prestart.sh
+++ prestart.sh
@@ -9,23 +9,9 @@ done
 
 echo 'Creating and migrating database'
 # in spin the database already exists
-PGDATABASE=postgres psql -c "create database nmdc_a;" \
-|| pg_dump \
-    --table "user_logins" \
-    --table "submission_metadata" \
-    --table "file_download" \
-    --file "nmdc_a-$(date +%s)-$(openssl rand -hex 32).sql" \
-    --data-only \
-    nmdc_a
+PGDATABASE=postgres psql -c "create database nmdc_a;" || true
 
 
-PGDATABASE=postgres psql -c "create database nmdc_b;" \
-|| pg_dump \
-    --table "user_logins" \
-    --table "submission_metadata" \
-    --table "file_download" \
-    --file "nmdc_b-$(date +%s)-$(openssl rand -hex 32).sql" \
-    --data-only \
-    nmdc_b
+PGDATABASE=postgres psql -c "create database nmdc_b;" || true
 
 nmdc-server migrate
