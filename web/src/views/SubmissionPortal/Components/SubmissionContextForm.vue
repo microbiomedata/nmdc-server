d8e2601981362e8e2109674f8524f532e0910f30
naglepuff <mike.nagler@kitware.com>
2023-02-03 16:11:58 -0500

    Make spacing between checkboxes smaller
    Keeps spacing more consistent with the radion buttons right above.

--- web/src/views/SubmissionPortal/Components/SubmissionContextForm.vue
+++ web/src/views/SubmissionPortal/Components/SubmissionContextForm.vue
@@ -86,6 +86,7 @@ export default defineComponent({
         />
         <v-checkbox
           v-model="contextForm.facilities"
+          class="mt-2"
           label="JGI"
           value="JGI"
           hide-details
