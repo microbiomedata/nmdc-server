fca4416cc3e95f16db456f53b3c3914c8f52784b
Patrick Kalita <pkalita@lbl.gov>
2023-03-17 16:16:54 -0700

    Implement WordPress-style app bar

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,11 +1,11 @@
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
-import AuthButton from '@/components/Presentation/AuthButton.vue';
+import AppHeader from '@/components/Presentation/AppHeader.vue';
 import { setConditions, stateRefs } from '@/store/';
 
 export default defineComponent({
   name: 'App',
-  components: { AuthButton },
+  components: { AppHeader },
   setup() {
     return {
       setConditions,
@@ -17,111 +17,7 @@ export default defineComponent({
 
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="white"
-      clipped-left
-      elevation="1"
-    >
-      <a
-        style="height: 100%"
-        @click="setConditions([])"
-      >
-        <img
-          src="/NMDC_logo_long.jpg"
-          height="100%"
-          alt="Page home"
-        >
-      </a>
-      <v-spacer />
-
-      <v-btn
-        depressed
-        small
-        href="https://nmdc-documentation.readthedocs.io/en/latest/tutorials/nav_data_portal.html"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="mx-2"
-      >
-        Video Tutorial
-        <v-icon
-          class="pl-2"
-          small
-        >
-          mdi-open-in-new
-        </v-icon>
-      </v-btn>
-
-      <v-btn
-        depressed
-        small
-        href="https://nmdc-documentation.readthedocs.io/en/latest/howto_guides/portal_guide.html"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="mx-2"
-      >
-        User Guide
-        <v-icon
-          class="pl-2"
-          small
-        >
-          mdi-open-in-new
-        </v-icon>
-      </v-btn>
-
-      <v-btn
-        depressed
-        small
-        href="https://nmdc-documentation.readthedocs.io/en/latest/index.html"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="mx-2"
-      >
-        NMDC Docs
-        <v-icon
-          class="pl-2"
-          small
-        >
-          mdi-open-in-new
-        </v-icon>
-      </v-btn>
-
-      <v-btn
-        depressed
-        small
-        href="https://microbiomedata.org/"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="mx-2"
-      >
-        NMDC Home
-        <v-icon
-          class="pl-2"
-          small
-        >
-          mdi-open-in-new
-        </v-icon>
-      </v-btn>
-
-      <v-divider vertical />
-
-      <v-btn
-        depressed
-        small
-        color="secondary"
-        :to="{ name: 'Submission Home' }"
-        class="mx-2"
-      >
-        Submission Portal
-        <v-icon
-          class="pl-2"
-          small
-        >
-          mdi-form-select
-        </v-icon>
-      </v-btn>
-      <AuthButton />
-    </v-app-bar>
+    <app-header @logoClicked="setConditions([])" />
     <keep-alive>
       <router-view />
     </keep-alive>
---
+++ web/src/menus.ts
@@ -0,0 +1,124 @@
+const Menus = [
+  {
+    label: 'About',
+    href: 'https://microbiomedata.org/about/',
+    items: [
+      {
+        label: 'Team',
+        href: 'https://microbiomedata.org/team/',
+      },
+      {
+        label: 'Advisory',
+        href: 'https://microbiomedata.org/advisory/',
+      },
+      {
+        label: 'IDEA Strategic Plan',
+        href: 'https://microbiomedata.org/idea-strategic-plan/',
+      },
+      {
+        label: 'Code of Conduct',
+        href: 'https://microbiomedata.org/nmdc-code-of-conduct/',
+      },
+      {
+        label: 'Acknowledgements',
+        href: 'https://microbiomedata.org/acknowledgements/',
+      },
+    ],
+  },
+  {
+    label: 'Data',
+    href: 'https://microbiomedata.org/data/',
+    items: [
+      {
+        label: 'Metadata',
+        href: 'https://microbiomedata.org/metadata/',
+      },
+      {
+        label: 'Workflows',
+        href: 'https://microbiomedata.org/workflows/',
+      },
+      {
+        label: 'Fair',
+        href: 'https://microbiomedata.org/fair/',
+      },
+      {
+        label: 'Data Use Policy',
+        href: 'https://microbiomedata.org/nmdc-data-use-policy/',
+      },
+    ],
+  },
+  {
+    label: 'Products',
+    items: [
+      {
+        label: 'Data Portal',
+        to: '/',
+      },
+      {
+        label: 'Submission Portal',
+        to: '/submission/home',
+      },
+      {
+        label: 'NMDC EDGE',
+        href: 'https://nmdc-edge.org',
+      },
+    ],
+  },
+  {
+    label: 'Community',
+    href: 'https://microbiomedata.org/community/',
+    items: [
+      {
+        label: 'Ambassadors',
+        href: 'https://microbiomedata.org/ambassadors/',
+      },
+      {
+        label: 'Champions',
+        href: 'https://microbiomedata.org/community/championsprogram/',
+      },
+      {
+        label: 'Community Conversations',
+        href: 'https://microbiomedata.org/community/community-conversations/',
+      },
+      {
+        label: 'Events',
+        href: 'https://microbiomedata.org/events/',
+      },
+      {
+        label: 'News',
+        href: 'https://microbiomedata.org/news/',
+      },
+    ],
+  },
+  {
+    label: 'Resources',
+    items: [
+      {
+        label: 'Annual Reports',
+        href: 'https://microbiomedata.org/annual_report/',
+      },
+      {
+        label: 'Media Materials',
+        href: 'https://microbiomedata.org/media/',
+      },
+      {
+        label: 'Data Management',
+        href: 'https://microbiomedata.org/data-management/',
+      },
+      {
+        label: 'Letter of Support',
+        href: 'https://docs.google.com/forms/d/e/1FAIpQLScF4yChmxXZG1v1fXa-2ydyN8QEyTYsKYd9jWICcrwdaG222A/viewform?usp=sf_link',
+      },
+      {
+        label: 'Publications',
+        href: 'https://microbiomedata.org/publications/',
+      },
+    ],
+  },
+  {
+    label: 'Contact',
+    href: 'https://microbiomedata.org/contact/',
+  },
+];
+
+export default Menus;
