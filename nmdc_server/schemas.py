280610e24c6d2c1641317022693e0900a75fd9c5
naglepuff <mike.nagler@kitware.com>
2023-03-27 12:44:25 -0400

    Add explicit gold identifiers column

--- nmdc_server/schemas.py
+++ nmdc_server/schemas.py
@@ -224,6 +224,7 @@ class StudyBase(AnnotatedBase):
     funding_sources: Optional[List[str]]
     ess_dive_datasets: Optional[List[str]]
     massive_study_identifiers: Optional[List[str]]
+    gold_study_identifiers: Optional[List[str]]
 
     @validator("principal_investigator_websites", pre=True, each_item=True)
     def replace_websites(cls, study_website: Union[models.StudyWebsite, str]) -> str:
