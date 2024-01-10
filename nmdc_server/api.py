1e67652ed78060fd0e10e8d00151b3c0be0714c2
eecavanna <eecavanna@users.noreply.github.com>
2024-01-10 12:56:04 -0800

    Reorder `import` module names to appease linter

--- nmdc_server/api.py
+++ nmdc_server/api.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import Session
 from starlette.requests import Request
 from starlette.responses import RedirectResponse, StreamingResponse
 
-from nmdc_server import crud, jobs, models, query, schemas, schemas_submission, __version__
+from nmdc_server import __version__, crud, jobs, models, query, schemas, schemas_submission
 from nmdc_server.auth import (
     admin_required,
     get_current_user,
