1a3eea5b902e84e6fd79e87da9aeb72b59c8a965
Mary Salvi <mary.salvi@kitware.com>
2024-02-29 18:08:15 -0500

    Update Bio component
    Remove PI's from Consortia. Limit team to 3 lines and scroll overflow

--- web/src/components/InvestigatorBio.vue
+++ web/src/components/InvestigatorBio.vue
@@ -57,56 +57,63 @@ export default defineComponent({
         style="height: 100%"
       >
         <v-card flat>
-          <div class="text-h3">
-            {{ item.principal_investigator_name }}
-          </div>
-          <div class="text-h5 py-2">
-            Principal investigator
-          </div>
-          <span
-            v-if="hasOrcid"
-            style="display: flex; align-items: center;"
-            class="py-1"
-          >
-            <orcid-id
-              :orcid-id="getOrcid(item.principal_investigator)"
-              :authenticated="false"
-              :width="24"
-            />
-          </span>
-          <div class="text-h5 py-2 primary--text">
-            Team
-          </div>
-          <v-menu
-            v-for="member in team"
-            :key="member.applies_to_person.orcid"
-            bottom
-            max-width="450px"
-            nudge-bottom="34"
-          >
-            <template #activator="{ on, attrs }">
-              <div
-                class="text-subtitle-1 px-1 grey--text text--darken-2"
-                style="display: inline-block; text-decoration: underline;"
-                v-bind="attrs"
-                v-on="on"
-              >
-                {{ member.applies_to_person.name }}
-              </div>
-            </template>
-            <v-card
-              class=" d-flex flex-column justify-start pa-2"
+          <div v-if="item.study_category != 'consortium'">
+            <div class="text-h3">
+              {{ item.principal_investigator_name }}
+            </div>
+            <div class="text-h5 py-2">
+              Principal investigator
+            </div>
+            <span
+              v-if="hasOrcid"
+              style="display: flex; align-items: center;"
+              class="py-1"
             >
-              <v-card-title>{{ member.applies_to_person.name }}</v-card-title>
-              <v-card-subtitle>CRediT: {{ member.applied_roles.join(', ') }}</v-card-subtitle>
               <orcid-id
-                v-if="member.applies_to_person.orcid"
-                :orcid-id="getOrcid(member)"
+                :orcid-id="getOrcid(item.principal_investigator)"
                 :authenticated="false"
                 :width="24"
               />
-            </v-card>
-          </v-menu>
+            </span>
+          </div>
+          <div
+            v-if="team"
+            class="text-h5 py-2 primary--text"
+          >
+            Team
+          </div>
+          <div class="team">
+            <v-menu
+              v-for="member in team"
+              :key="member.applies_to_person.orcid"
+              bottom
+              max-width="450px"
+              nudge-bottom="34"
+            >
+              <template #activator="{ on, attrs }">
+                <div
+                  class="text-subtitle-1 px-1 grey--text text--darken-2"
+                  style="display: inline-block; text-decoration: underline;"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  {{ member.applies_to_person.name }}
+                </div>
+              </template>
+              <v-card
+                class=" d-flex flex-column justify-start pa-2"
+              >
+                <v-card-title>{{ member.applies_to_person.name }}</v-card-title>
+                <v-card-subtitle>CRediT: {{ member.applied_roles.join(', ') }}</v-card-subtitle>
+                <orcid-id
+                  v-if="member.applies_to_person.orcid"
+                  :orcid-id="getOrcid(member)"
+                  :authenticated="false"
+                  :width="24"
+                />
+              </v-card>
+            </v-menu>
+          </div>
           <div class="text-h5 py-2 primary--text">
             Funding Sources
           </div>
@@ -118,3 +125,13 @@ export default defineComponent({
     </v-col>
   </v-row>
 </template>
+
+<style>
+.team {
+-webkit-line-clamp: 3;
+display: -webkit-box;
+-webkit-box-orient: vertical;
+overflow-y: auto;
+width: fit-content;
+}
+</style>
