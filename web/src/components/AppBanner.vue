0c20b68582a72859e2c28d95c8c98ffb28391963
eecavanna <134325062+eecavanna@users.noreply.github.com>
2024-04-18 16:12:17 -0700

    Remove underlined whitespace following hyperlinks

--- web/src/components/AppBanner.vue
+++ web/src/components/AppBanner.vue
@@ -8,14 +8,12 @@
   >
     <span class="title">Announcement: </span>The NMDC will be replacing legacy identifiers with
     <a href="https://microbiomedata.github.io/nmdc-schema/identifiers/#ids-minted-for-use-within-nmdc">
-      NMDC persistent identifiers
-    </a> on April 22, 2024 for studies,
+      NMDC persistent identifiers</a> on April 22, 2024 for studies,
     samples, analysis workflows, and files. Additionally, annotation will be reprocessed for legacy metagenomes
     and metatranscriptomes, along with binning for metagenomes. Legacy metaproteomics datasets will be reprocessed
     using the new metagenome annotations. For information on mapping between old and new identifiers please refer
     to
     <a href="https://microbiomedata.github.io/nmdc-schema/identifiers/#reuse-vs-minting-new-ids">
-      this documentation
-    </a>. If you have questions please contact support@microbiomedata.org.
+      this documentation</a>. If you have questions please contact support@microbiomedata.org.
   </v-banner>
 </template>
