f2c0c0de75d1c151f7300567502f0c2a2ac096c2
naglepuff <mike.nagler@kitware.com>
2024-04-18 12:06:08 -0400

    Filter out irrelevant omics_processing results

--- nmdc_server/api.py
+++ nmdc_server/api.py
@@ -189,9 +189,15 @@ async def search_biosample(
                     da.selected = schemas.DataObject.is_selected(workflow, da, data_object_filter)
         return biosample
 
-    return pagination.response(
+    results = pagination.response(
         crud.search_biosample(db, query.conditions, data_object_filter), insert_selected
     )
+    biosample_ids = [b.id for b in results["results"]]
+    omics_results = crud.search_omics_processing_for_biosamples(db, query.conditions, biosample_ids)
+    omics_ids = set([str(o.id) for o in omics_results.all()])
+    for biosample in results["results"]:
+        biosample.omics_processing = [op for op in biosample.omics_processing if op.id in omics_ids]
+    return results
 
 
 @router.post(
