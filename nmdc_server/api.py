aac47e22cf86bb3149a175ec2810084599561878
James Tessmer <james.tessmer@pnnl.gov>
2024-08-06 12:21:54 -0700

    Fix linting errors

--- nmdc_server/api.py
+++ nmdc_server/api.py
@@ -783,7 +783,7 @@ def create_github_issue(submission, user):
     omicsprocessingtypes = ", ".join(multiomicsform["omicsProcessingTypes"])
     sampletype = ", ".join(submission.metadata_submission["templates"])
     sampledata = submission.metadata_submission["sampleData"]
-    fundingSource = studyform["fundingSource"]
+    fundingsource = studyform["fundingSource"]
     numsamples = 0
     for key in sampledata:
         numsamples = max(numsamples, len(sampledata[key]))
@@ -813,7 +813,7 @@ def create_github_issue(submission, user):
         f"Data types: {omicsprocessingtypes}",
         f"Sample type:{sampletype}",
         f"Number of samples:{numsamples}",
-        f"Funding source:{fundingSource}",
+        f"Funding source:{fundingsource}",
     ] + valid_ids
     body_string = " \n ".join(body_lis)
     payload_dict = {
