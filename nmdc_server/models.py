7e456d2dc60576894d24805968892546716c9772
Mary Salvi <mary.salvi@kitware.com>
2024-10-17 15:15:23 -0400

    Add email to user model

--- nmdc_server/models.py
+++ nmdc_server/models.py
@@ -864,6 +864,7 @@ class User(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     orcid = Column(String, nullable=False)
     name = Column(String)
+    email = Column(String, nullable=True)
     is_admin = Column(Boolean, nullable=False, default=False)
 
 
