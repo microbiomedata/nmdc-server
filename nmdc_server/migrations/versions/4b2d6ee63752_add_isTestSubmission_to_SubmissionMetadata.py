2a5f681fb24252107c5294d85d514f571a4294d3
James Tessmer <james.tessmer@pnnl.gov>
2025-01-15 11:52:22 -0800

    update down version number in migrator

--- nmdc_server/migrations/versions/4b2d6ee63752_add_isTestSubmission_to_SubmissionMetadata.py
+++ nmdc_server/migrations/versions/4b2d6ee63752_add_isTestSubmission_to_SubmissionMetadata.py
@@ -3,7 +3,7 @@ This field added as a change to enable the differentiation between test submissi
 (those done in training/at a conference demo) and real submissions
 
 Revision ID: 4b2d6ee63752
-Revises: 605ae36be6b5
+Revises: afa1ff687968
 Create Date: 2025-01-03 18:07:09.029643
 
 """
@@ -15,7 +15,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "4b2d6ee63752"
-down_revision: Optional[str] = "605ae36be6b5"
+down_revision: Optional[str] = "afa1ff687968"
 branch_labels: Optional[str] = None
 depends_on: Optional[str] = None
 
