264652bee73403cc511b021b1e93c028b98217bb
eecavanna <eecavanna@users.noreply.github.com>
2025-01-23 15:42:07 -0800

    Show app banner about January 26-February 3 maintenance period

--- web/src/components/AppBanner.vue
+++ web/src/components/AppBanner.vue
@@ -3,7 +3,7 @@ import { defineComponent, ref } from '@vue/composition-api';
 
 export default defineComponent({
   setup() {
-    const showAppBanner = ref(false);
+    const showAppBanner = ref(true);
     return { showAppBanner };
   },
 });
@@ -20,10 +20,20 @@ export default defineComponent({
     <p
       class="title"
     >
-      Banner title (replace me)
+      Upcoming maintenance (January 26-February 3)
     </p>
     <p>
-      Banner content (replace me)
+      From Sunday, January 26 to Sunday, February 2, our data storage system will be
+      undergoing scheduled maintenance. During that time, the downloading of data files
+      will be disabled.
+    </p>
+    <p>
+      Then, on Monday, February 3, the NMDC Data Portal and NMDC Submission Portal will be
+      undergoing software updates. During that time, both portals will be offline.
+    </p>
+    <p>
+      Both portals will be back online and fully functional by Monday, February 3,
+      at 5:00 p.m. Pacific time.
     </p>
   </v-banner>
 </template>
