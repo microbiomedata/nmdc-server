263eed82a451e70e0bbe21183f42bde6c931971b
naglepuff <mike.nagler@kitware.com>
2025-03-28 11:14:48 -0400

    Fix bug

--- nmdc_server/ingest/omics_processing.py
+++ nmdc_server/ingest/omics_processing.py
@@ -106,7 +106,9 @@ def get_configuration_name(mongodb: Database, configuration_id: str, config_map)
     return config_record["name"] if config_record else None
 
 
-def get_poolable_replicate_manifest(omics_processing, data_object_id, mongodb: Database) -> str:
+def get_poolable_replicate_manifest(
+    omics_processing_id: str, data_object_id: str, mongodb: Database
+) -> str:
     """
     Determine which poolable replicate manifest, if any, this data_generation is associated with.
 
@@ -115,12 +117,16 @@ def get_poolable_replicate_manifest(omics_processing, data_object_id, mongodb: D
     """
     data_object_document = mongodb["data_object_set"].find_one({"id": data_object_id})
     if not data_object_document or not data_object_document.get("in_manifest", None):
-        return omics_processing["id"]
+        return omics_processing_id
+
     manifest_id = data_object_document.get("in_manifest")[0]
-    manifest_document = mongodb["manifest_set"].find_one({"id": data_object_id})
+    manifest_document = mongodb["manifest_set"].find_one({"id": manifest_id})
+    if manifest_document:
+        print(manifest_id, manifest_document["manifest_category"])
     if manifest_document and manifest_document["manifest_category"] == "poolable_replicates":
+        print(manifest_document)
         return manifest_id
-    return omics_processing["id"]
+    return omics_processing_id
 
 
 def load_omics_processing(db: Session, obj: Dict[str, Any], mongodb: Database, logger, config_map):
@@ -189,7 +195,7 @@ def load_omics_processing(db: Session, obj: Dict[str, Any], mongodb: Database, l
         omics_processing.outputs.append(data_object)  # type: ignore
 
         manifest_id: str = get_poolable_replicate_manifest(
-            omics_processing, data_object_id, mongodb
+            omics_processing.id, data_object_id, mongodb
         )
 
     omics_processing.poolable_replicates_manifest_id = manifest_id
