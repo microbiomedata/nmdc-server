2800985c1dacdd62362f0fab65920baa3c91b6ce
Yan Xu <yxu@pn2405079.lanl.gov>
2025-05-08 15:42:30 -0600

    remove "used" columns and "best_protein" column

--- nmdc_server/models.py
+++ nmdc_server/models.py
@@ -685,8 +685,6 @@ nom_analysis_output_association = output_association("nom_analysis")
 class NOMAnalysis(Base, PipelineStep):
     __tablename__ = "nom_analysis"
 
-    used = Column(String, nullable=False)
-
     inputs = input_relationship(nom_analysis_input_association)
     outputs = output_relationship(nom_analysis_output_association)
 
@@ -721,8 +719,6 @@ metabolomics_analysis_output_association = output_association("metabolomics_anal
 
 class MetabolomicsAnalysis(Base, PipelineStep):
     __tablename__ = "metabolomics_analysis"
-
-    used = Column(String, nullable=False)
 
     inputs = input_relationship(metabolomics_analysis_input_association)
     outputs = output_relationship(metabolomics_analysis_output_association)
@@ -830,7 +826,6 @@ class MetaPGeneFunctionAggregation(Base):
     )
     gene_function_id = Column(String, ForeignKey(GeneFunction.id), primary_key=True)
     count = Column(BigInteger, nullable=False)
-    best_protein = Column(Boolean, nullable=False)
 
 
 class MetaTGeneFunctionAggregation(Base):
