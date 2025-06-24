23a7e17271e55e038890b76cf31fc0c512fb16a6
James Tessmer <james.tessmer@pnnl.gov>
2025-06-24 13:35:02 -0700

    Small fix to awardDois migrator

--- nmdc_server/migrations/versions/6cb0b331ce8c_.py
+++ nmdc_server/migrations/versions/6cb0b331ce8c_.py
@@ -5,6 +5,7 @@ Revises: 997064dd504d
 Create Date: 2025-06-24 19:42:22.295714
 
 """
+
 from typing import Optional
 from uuid import uuid4
 
@@ -15,8 +16,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 # revision identifiers, used by Alembic.
-revision: str = '6cb0b331ce8c'
-down_revision: Optional[str] = '997064dd504d'
+revision: str = "6cb0b331ce8c"
+down_revision: Optional[str] = "997064dd504d"
 branch_labels: Optional[str] = None
 depends_on: Optional[str] = None
 
@@ -25,6 +26,7 @@ class SubmissionMetadata(Base):
     __tablename__ = "submission_metadata"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     metadata_submission = Column(JSONB, nullable=False)
+
 
 def upgrade():
     session = orm.Session(bind=op.get_bind())
@@ -35,19 +37,16 @@ def upgrade():
         if isinstance(metadata_submission, list):
             continue
 
-        multiOmicsForm = metadata_submission["multiOmicsForm"]
+        multi_omics_form = metadata_submission["multiOmicsForm"]
 
-        if multiOmicsForm.get("awardDois", None):
-            values = multiOmicsForm["awardDois"]
+        if multi_omics_form.get("awardDois", None):
+            values = multi_omics_form["awardDois"]
             values = values.split(",")
             new_dois = []
             for val in values:
-                new_dois.append({
-                    "value": val,
-                    "provider": ''
-                })
-    
-            multiOmicsForm["awardDois"] = new_dois
+                new_dois.append({"value": val, "provider": ""})
+
+            multi_omics_form["awardDois"] = new_dois
         mappings.append({"id": submission_metadata.id, "metadata_submission": metadata_submission})
 
     session.bulk_update_mappings(SubmissionMetadata, mappings)
@@ -64,15 +63,15 @@ def downgrade():
         if isinstance(metadata_submission, list):
             continue
 
-        multiOmicsForm = metadata_submission["multiOmicsForm"]
+        multi_omics_form = metadata_submission["multiOmicsForm"]
 
-        if multiOmicsForm.get("awardDois", None):
-            values = multiOmicsForm["awardDois"]
+        if multi_omics_form.get("awardDois", None):
+            values = multi_omics_form["awardDois"]
             new_dois = []
             for dict in values:
-                new_dois.append(dict['value'])
-    
-            multiOmicsForm["awardDois"] = new_dois
+                new_dois.append(dict["value"])
+
+            multi_omics_form["awardDois"] = new_dois
         mappings.append({"id": submission_metadata.id, "metadata_submission": metadata_submission})
 
     session.bulk_update_mappings(SubmissionMetadata, mappings)
