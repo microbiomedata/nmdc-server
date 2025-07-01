3fe6a65dbb2898896d495709de11867f0caf6136
James Tessmer <james.tessmer@pnnl.gov>
2025-07-01 10:57:36 -0700

    update DOI hints

--- web/src/definitions.ts
+++ web/src/definitions.ts
@@ -17,8 +17,8 @@ const Definitions = {
   studyNCBIBioProjectAccession: 'Provide the NCBI BioProject Accession Number associated with the listed NCBI BioProject Title.',
   metadataTypes: 'Check all -omics data types associated with samples collected for this study.',
   doi: 'DOI associated with the user project awarded. This is required when data is already generated and can be found <a href="https://www.osti.gov/award-doi-service/" target="_blank" rel="noopener noreferrer">here</a>.',
-  dataDoiProvider: 'This is a placeholder hint for dataDoiProvider and should be replaced',
-  dataDoiValue: 'This is a placeholder hint for dataDoiValue and should be replaced',
+  dataDoiProvider: 'The authority, or organization, that created and provided the DOI.',
+  dataDoiValue: 'A type of DOI that resolves to generated data.',
   unknownDoi: 'If selected you will not have to enter the required Award DOIs and you will be contacted by an NMDC team member after you have submitted your data.',
 };
 
