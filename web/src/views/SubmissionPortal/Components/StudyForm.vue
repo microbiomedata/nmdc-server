10751d88a6c9a7de1437782647be1f9f41ba24ae
samobermiller <samobermiller@gmail.com>
2025-09-30 17:00:17 -0700

    if submission uneditable, add pop up box with email contact

--- web/src/views/SubmissionPortal/Components/StudyForm.vue
+++ web/src/views/SubmissionPortal/Components/StudyForm.vue
@@ -157,6 +157,30 @@ export default defineComponent({
       type="info"
       class="ma-2"
     >
+      <template #prepend>
+        <v-menu
+          bottom
+          offset-y
+          :close-on-content-click="false"
+          max-width="300"
+        >
+          <template #activator="{ on, attrs }">
+            <v-icon
+              v-bind="attrs"
+              style="cursor: pointer;"
+              v-on="on"
+            >
+              mdi-information
+            </v-icon>
+          </template>
+          <v-card>
+            <v-card-text>
+              If you need to edit this submission, please contact:
+              <span style="user-select: all; cursor: text; font-weight: bold;">support@microbiomedata.org</span>
+            </v-card-text>
+          </v-card>
+        </v-menu>
+      </template>
       This submission has status "{{ SubmissionStatusTitleMapping[status] }}" and cannot be edited.
     </v-alert>
     <v-form
