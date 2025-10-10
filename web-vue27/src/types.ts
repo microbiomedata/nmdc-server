4184601fb3c678a0633b5e51027e51ac5f080b01
Cody O'Donnell <ctodonnell@lbl.gov>
2025-10-10 14:20:25 -0700

    Remove backup web-vue27 directory

--- web-vue27/src/types.ts
+++
@@ -1,7 +0,0 @@
-export interface User{
-    id: string,
-    orcid: string;
-    name: string;
-    is_admin: boolean;
-    email?: string;
-}
--- web-vue27/src/views/SubmissionPortal/Components/SubmissionPermissionBanner.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <v-alert
-    type="info"
-  >
-    Your current permission level for this submission does not allow editing of this page. Contact the submission author to request a change in permission level.
-  </v-alert>
-</template>
