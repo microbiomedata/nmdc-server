e767b2d230471b1e5e1a3056225f4a383ba41041
Patrick Kalita <pkalita@lbl.gov>
2025-10-10 11:59:16 -0700

    Serve pre-generated JSON schema file from static directory

--- nmdc_server/static_files.py
+++ nmdc_server/static_files.py
@@ -26,25 +26,15 @@ def initialize_static_directory(*, remove_existing=False) -> Path:
 
 
 def generate_submission_schema_files(directory: Path) -> None:
+    """Copy artifacts from nmdc_submission_schema package to the static directory."""
     submission_schema_files = importlib.resources.files("nmdc_submission_schema")
-
-    # Load each class in the submission schema, ensure that each slot of the class
-    # is fully materialized into attributes, and then drop the slot usage definitions
-    # to save some bytes.
-    schema_path = submission_schema_files / "schema/nmdc_submission_schema.yaml"
-    sv = SchemaView(str(schema_path))
-    for class_name, class_definition in sv.all_classes().items():
-        for slot_definition in sv.class_induced_slots(class_name):
-            class_definition.attributes[slot_definition.name] = slot_definition
-    sv.materialize_patterns()
-    for class_definition in sv.all_classes().values():
-        class_definition.slot_usage = None
 
     out_dir = directory / "submission_schema"
     out_dir.mkdir(exist_ok=True)
 
-    # The entire submission schema in JSON format
-    json_dumper.dump(sv.schema, out_dir / "submission_schema.json")
+    # The schema itself in JSON format
+    schema_path = submission_schema_files / "project/json/nmdc_submission_schema.json"
+    shutil.copyfile(str(schema_path), out_dir / "submission_schema.json")
 
     # The GOLD ecosystem tree that the submission schema re-exports
     gold_tree_path = submission_schema_files / "project/thirdparty/GoldEcosystemTree.json"
