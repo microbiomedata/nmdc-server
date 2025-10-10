ce79e1ca1f7489e93335b6dd8c11172c4a0fe61d
Cody O'Donnell <ctodonnell@lbl.gov>
2025-10-10 14:15:31 -0700

    Add types, data, definitions, encoding, menus, and schema files

---
+++ web/src/definitions.ts
@@ -0,0 +1,25 @@
+const Definitions = {
+  studyName: 'A Study Name is required. We recommend the use of standardized, informative study names as described by the GOLD team (Mukherjee et. al., 2023).',
+  piName: 'The Principal Investigator who led the study and/or generated the data.',
+  piEmail: 'The Principal Investigator\'s email is required for submission. The submitter does not have to be the Principal Investigator.',
+  piOrcid: '<a href="https://orcid.org" target="_blank" rel="noopener noreferrer">ORCID iD</a> of the Principal Investigator.',
+  fundingSources: 'The Funding Source of the research performed under this study should be provided and include an awarding program and the award or grant number. See an <a href="https://data.microbiomedata.org/details/study/nmdc:sty-11-8fb6t785" target="_blank" rel="noopener noreferrer">existing study</a> for an example.',
+  linkOutWebpage: 'Optionally, institute, project, or research lab weblinks can be provided. If your research group or program has a website to share, please include the links.',
+  studyDescription: 'The Study Description should provide a summary that includes the context of your study, research, goals, and design. We recommend this be similar to abstract format, describing the entire study similar to how an abstract describes a single publication.',
+  studyOptionalNotes: 'Any additional information that is of value to your study can be provided here.',
+  studyContributors: 'Other researchers associated with this study.',
+  contributorFullName: 'Contributor\'s name associated with this study. First name, Middle initial, Last name',
+  contributorRoles: 'Identify all <a href="https://credit.niso.org/" target="_blank" rel="noopener noreferrer">CRediT roles</a> associated with this contributor. <a href="https://info.orcid.org/credit-for-research-contribution" target="_blank" rel="noopener noreferrer">ORCID and CRediT Information</a>.',
+  contributorOrcid: '<a href="https://orcid.org" target="_blank" rel="noopener noreferrer">ORCID iD</a> of the contributor identified.',
+  studyAlternativeNames: 'Project, study, or sample set names the are also associated with this submission or other names / identifiers for this study.',
+  studyGoldID: 'Provide the GOLD study IDs associated with samples for this study.',
+  studyNCBIProjectTitle: 'Provide the NCBI BioProject Title(s) associated with this study.',
+  studyNCBIBioProjectAccession: 'Provide the NCBI BioProject Accession Number associated with the listed NCBI BioProject Title.',
+  metadataTypes: 'Check all -omics data types associated with samples collected for this study.',
+  doi: 'DOI associated with the user project awarded. This is required when data is already generated and can be found <a href="https://www.osti.gov/award-doi-service/" target="_blank" rel="noopener noreferrer">here</a>.',
+  dataDoiProvider: 'The authority, or organization, that created and provided the DOI.',
+  dataDoiValue: 'A type of DOI that resolves to generated data.',
+  unknownDoi: 'If selected you will not have to enter the required Award DOIs and you will be contacted by an NMDC team member after you have submitted your data.',
+};
+
+export default Definitions;
