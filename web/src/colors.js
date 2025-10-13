d32edaddad912f41a97906a8e3957951764210e9
Cody O'Donnell <ctodonnell@lbl.gov>
2025-10-13 12:26:15 -0700

    Add colors file

---
+++ web/src/colors.js
@@ -0,0 +1,27 @@
+import colors from 'vuetify/lib/util/colors';
+
+const red = '#ED5338';
+const orange = '#E88320';
+const green = colors.lightGreen.darken2;
+const blue = '#00AAE7';
+const purple = '#4F3B80';
+
+export default {
+  red,
+  orange,
+  green,
+  blue,
+  purple,
+  grey: colors.grey,
+  primary: purple,
+  secondary: red,
+  accent: orange,
+  aquatic: blue,
+  terrestrial: green,
+  hostAssociated: red,
+  engineered: orange,
+  metagenome: blue,
+  metatranscriptome: green,
+  sequencing: red,
+  success: green,
+};
