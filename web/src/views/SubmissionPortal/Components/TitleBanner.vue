00f16faec458c2bf7edcb6af92a43cf6c208a464
Cody O'Donnell <ctodonnell@lbl.gov>
2025-11-03 13:59:43 -0800

    Refactor submission portal banner to use vite features

--- web/src/views/SubmissionPortal/Components/TitleBanner.vue
+++ web/src/views/SubmissionPortal/Components/TitleBanner.vue
@@ -2,18 +2,21 @@
 import {
   defineComponent,
 } from 'vue';
+import heroImage from '@/assets/submission-portal-hero.png';
 
 export default defineComponent({
   components: {},
   setup() {
-    return {};
+    return {
+      heroImage,
+    };
   },
 });
 </script>
 
 <template>
   <v-container>
-    <v-row :style="{ backgroundImage: 'url(' + require('@/assets/submission-portal-hero.png') + ')', backgroundSize: 'cover' }">
+    <v-row :style="{ backgroundImage: `url(${heroImage})`, backgroundSize: 'cover' }">
       <v-col class="text-center py-16">
         <h1 class="white--text">
           Submission Portal
