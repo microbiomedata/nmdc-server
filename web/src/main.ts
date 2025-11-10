5848961f6993fe7647ccce03909b8e98e73b90ee
Cody O'Donnell <ctodonnell@lbl.gov>
2025-11-10 14:22:08 -0800

    Reintroduce and upgrade sentry

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,6 +4,7 @@
  * Bootstraps Vuetify and other plugins then mounts the App`
  */
 import { createApp } from 'vue'
+import * as Sentry from "@sentry/vue";
 import { registerPlugins } from '@/plugins'
 import App from './App.vue'
 import 'unfonts.css'
@@ -11,5 +12,13 @@ import 'unfonts.css'
 export const app = createApp(App)
 
 registerPlugins(app)
+
+if (import.meta.env.PROD) {
+  Sentry.init({
+    app,
+    dsn: 'https://87132695029c4406afe033fb3b13b115@o267860.ingest.sentry.io/5658761',
+    tracesSampleRate: 1.0,
+  });
+}
 
 app.mount('#app')
