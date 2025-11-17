fd0f5fb5b7ca8fc450026ae28ac550cd5f732667
samobermiller <samobermiller@gmail.com>
2025-11-17 10:54:10 -0800

    add migrator for updated submission status enum

---
+++ nmdc_server/migrations/versions/23ef8096759e_update_submissionstatus_values2.py
@@ -0,0 +1,55 @@
+"""Make submission status consistent with enum updates
+
+Revision ID: 23ef8096759e
+Revises: 9a0ff33a4e6b
+Create Date: 2025-11-17 18:41:13.169730
+
+"""
+from typing import Optional
+
+from alembic import op
+from nmdc_schema.nmdc import SubmissionStatusEnum
+import sqlalchemy as sa
+from sqlalchemy.sql import column, table
+
+
+# revision identifiers, used by Alembic.
+revision: str = '23ef8096759e'
+down_revision: Optional[str] = '9a0ff33a4e6b'
+branch_labels: Optional[str] = None
+depends_on: Optional[str] = None
+
+STATUS_MAP = [
+    {
+        "old": "ResubmittedPendingReview",
+        "new": SubmissionStatusEnum.SubmittedPendingReview.text,
+    },
+    {
+        "old": "InProgressUpdate",
+        "new": SubmissionStatusEnum.InProgress.text,
+    },
+    {
+        "old": "PendingUserFacility",
+        "new": SubmissionStatusEnum.ApprovedPendingUserFacility.text,
+    },
+]
+
+
+def upgrade():
+    data_object = table("submission_metadata", column("status", sa.String))
+    for mapping in STATUS_MAP:
+        op.execute(
+            data_object.update()
+            .where(data_object.c.status == mapping["old"])
+            .values(status=mapping["new"])
+        )
+
+
+def downgrade():
+    data_object = table("submission_metadata", column("status", sa.String))
+    for mapping in STATUS_MAP:
+        op.execute(
+            data_object.update()
+            .where(data_object.c.status == mapping["new"])
+            .values(status=mapping["old"])
+        )
