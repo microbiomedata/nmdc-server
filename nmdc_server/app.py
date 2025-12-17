aa6cd0d141c61867dbad8d800921fe463e6a9bda
eecavanna <eecavanna@users.noreply.github.com>
2025-12-16 17:36:21 -0800

    Check for a DSN _before_ invoking the initialization function

--- nmdc_server/app.py
+++ nmdc_server/app.py
@@ -20,15 +20,10 @@ from nmdc_server.swagger_ui.helpers import load_template
 
 def initialize_sentry():
     """
-    Initialize the Sentry SDK if the application is configured with a Sentry DSN.
+    Initialize the Sentry SDK.
     
     Reference: https://docs.sentry.io/concepts/key-terms/dsn-explainer/
     """
-
-    # Note: This application uses the truthiness of the DSN, itself, to determine
-    #       whether the application's administrator wants to use Sentry.
-    if not settings.sentry_dsn:
-        return
 
     sentry_sdk.init(
         dsn=settings.sentry_dsn,
@@ -83,7 +78,10 @@ def create_app(env: typing.Mapping[str, str]) -> FastAPI:
     async def redirect_docs():
         return "/api/docs"
 
-    initialize_sentry()
+    # Initialize Sentry if the application is configured with a Sentry DSN.
+    if settings.sentry_dsn:
+        initialize_sentry()
+
     errors.attach_error_handlers(app)
     app.include_router(api.router, prefix="/api")
     app.include_router(auth.router, prefix="/auth")
