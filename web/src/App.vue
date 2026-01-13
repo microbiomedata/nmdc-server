d572a9f57f2c61f7045571b75795c157d90d7ece
Cristina Stone Pedraza <cristina.stonepedraza@pnnl.gov>
2026-01-13 12:22:53 -0800

    Fix indentation

--- web/src/App.vue
+++ web/src/App.vue
@@ -17,12 +17,12 @@ export default defineComponent({
   setup() {
     const router = useRouter();
 
-const handleRefreshTokenExpired = () => {
-  stateRefs.user.value = null;
-  if (router) {
-    init(router, false);
-  }
-};
+    const handleRefreshTokenExpired = () => {
+      stateRefs.user.value = null;
+      if (router) {
+        init(router, false);
+      }
+    };
 
     const { user } = stateRefs;
 
@@ -40,29 +40,29 @@ const handleRefreshTokenExpired = () => {
     onMounted(async () => {
       window.addEventListener(REFRESH_TOKEN_EXPIRED_EVENT, handleRefreshTokenExpired);
 
-  if (!router) {
-    return;
-  }
-  if (router.currentRoute.value.path === '/login') {
-    // init() will be called in the LoginPage component in this case
-    return;
-  }
-  // The first time the app is loaded, there will not yet be an access token configured on
-  // the API client. There *may* be a refresh token in storage, so attempt to exchange it
-  // before calling the init() function. The exchangeRefreshToken function will throw a
-  // RefreshTokenExchangeError if the refresh token is invalid or expired. If that or any other
-  // exception is thrown during the exchange, we still want to call init() to finish setting up
-  // the app, but we don't want to re-throw the error.
-  try {
-    await api.exchangeRefreshToken();
-  } catch (error) {
-    if (!(error instanceof RefreshTokenExchangeError)) {
-      throw error;
-    }
-  } finally {
-    await init(router);
-  }
-});
+      if (!router) {
+        return;
+      }
+      if (router.currentRoute.value.path === '/login') {
+        // init() will be called in the LoginPage component in this case
+        return;
+      }
+      // The first time the app is loaded, there will not yet be an access token configured on
+      // the API client. There *may* be a refresh token in storage, so attempt to exchange it
+      // before calling the init() function. The exchangeRefreshToken function will throw a
+      // RefreshTokenExchangeError if the refresh token is invalid or expired. If that or any other
+      // exception is thrown during the exchange, we still want to call init() to finish setting up
+      // the app, but we don't want to re-throw the error.
+      try {
+        await api.exchangeRefreshToken();
+      } catch (error) {
+        if (!(error instanceof RefreshTokenExchangeError)) {
+          throw error;
+        }
+      } finally {
+        await init(router);
+      }
+    });
 
     return {
       stateRefs,
