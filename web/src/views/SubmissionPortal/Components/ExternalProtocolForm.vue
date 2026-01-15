56dd869265fd068dc6788c999ae1ba1757c4aa31
Patrick Kalita <pkalita@lbl.gov>
2026-01-15 08:45:05 -0800

    Bring text guidance out of tooltips for sample prep and data access sections

--- web/src/views/SubmissionPortal/Components/ExternalProtocolForm.vue
+++ web/src/views/SubmissionPortal/Components/ExternalProtocolForm.vue
@@ -102,211 +102,84 @@ export default defineComponent({
     flat
     multiple
   >
-    <v-expansion-panel>
-      <v-expansion-panel-title>
-        <template #actions>
-          <v-icon>
-            mdi-chevron-down
-          </v-icon>
-        </template>
-        <div>
-          Sample Preparation Protocol
-          <v-tooltip
-            right
-            class="x-2"
-            max-width="500"
-          >
-            <template #activator="{ props }">
-              <v-icon
-                v-bind="props"
-                class="ml-1 mb-1"
-                small
-              >
-                mdi-help-circle
-              </v-icon>
-            </template>
-            <span>
-              This protocol should describe how samples were extracted, digested (including which proteolytic enzyme
-              was used), and/or cleaned prior to analysis on an instrument. Provide a DOI, URL, or descriptive text.
-            </span>
-          </v-tooltip>
+    <v-expansion-panel title="Sample Preparation Protocol">
+      <v-expansion-panel-text class="text-body-2 px-8">
+        <div class="mb-4">
+          Use these fields to describe how samples were extracted, digested (including which proteolytic enzyme was
+          used), and/or cleaned prior to analysis on an instrument.
         </div>
-      </v-expansion-panel-title>
-      <v-expansion-panel-text>
-        <v-row
-          no-gutters
+        <v-checkbox
+          v-if="protocolNames.length > 0 && !currentProtocol.sampleProtocol.name"
+          v-model="currentProtocol.sampleProtocol.sharedData"
         >
-          <template
-            v-if="protocolNames.length > 0 && !currentProtocol.sampleProtocol.name"
-          >
-            <v-checkbox
-              v-model="currentProtocol.sampleProtocol.sharedData"
-              label="Protocol shared across data types"
-              class="mx-2"
-              color="primary"
-            >
-              <template #append>
-                <v-tooltip
-                  right
-                  class="x-2"
-                  max-width="500"
-                >
-                  <template #activator="{ props }">
-                    <v-icon
-                      v-bind="props"
-                    >
-                      mdi-help-circle
-                    </v-icon>
-                  </template>
-                  <span>
-                    If a previously described protocol was also followed for these analyses, include the shared protocol name provided in the previous step.
-                  </span>
-                </v-tooltip>
-              </template>
-            </v-checkbox>
+          <template #label>
+            <div>
+              <div>Use protocol described for other data types</div>
+              <div class="text-caption">
+                Select this option to reuse a protocol name already provided for another data type in this submission.
+              </div>
+            </div>
           </template>
-          <template
-            v-if="currentProtocol.sampleProtocol.sharedData && protocolNames.length > 0"
-          >
-            <v-col
-              cols="1"
-            />
-            <v-col
-              cols="4"
-            >
-              <v-select
-                v-model="currentProtocol.sampleProtocol.sharedDataName"
-                :items="protocolNames"
-                label="Select Protocol Name"
-                variant="outlined"
-                class="mx-2"
-                @change="updateMultiOmicsForm()"
-              />
-            </v-col>
-          </template>
-        </v-row>
-        <template
-          v-if="!currentProtocol.sampleProtocol.sharedData"
-        >
-          <v-row
-            class="mx-8 "
-            no-gutters
-          >
-            <v-col
-              cols="5"
-            >
+        </v-checkbox>
+        <v-select
+          v-if="currentProtocol.sampleProtocol.sharedData && protocolNames.length > 0"
+          v-model="currentProtocol.sampleProtocol.sharedDataName"
+          :items="protocolNames"
+          label="Select Protocol Name"
+          variant="outlined"
+          @change="updateMultiOmicsForm()"
+        />
+        <template v-if="!currentProtocol.sampleProtocol.sharedData">
+          <div class="mb-2">
+            <b>If a published protocol is available</b>, provide the DOI or URL here. The DOI or URL should link to publicly available documentation that describes the experimental protocol in detail, not for the research study publication.
+          </div>
+          <v-row class="mb-4">
+            <v-col>
               <v-text-field
                 v-model="currentProtocol.sampleProtocol.doi"
                 label="DOI"
                 variant="outlined"
                 :rules="doiValueRules()"
+                persistent-hint
+                hint="A DOI for the protocol"
                 @blur="updateMultiOmicsForm()"
-              >
-                <template #append>
-                  <v-tooltip
-                    right
-                    class="x-2"
-                    max-width="500"
-                  >
-                    <template #activator="{ props }">
-                      <v-icon
-                        v-bind="props"
-                      >
-                        mdi-help-circle
-                      </v-icon>
-                    </template>
-                    <span>
-                      Provide a DOI for the protocol. This is a DOI for publicly available documentation that describes the experimental protocol in detail, not for the research study publication. Multiple DOIs can be provided.              </span>
-                  </v-tooltip>
-                </template>
-              </v-text-field>
+              />
             </v-col>
-            <v-col
-              cols="5"
-              class="ml-4"
-            >
+            <v-col>
               <v-text-field
                 v-model="currentProtocol.sampleProtocol.url"
                 label="URL"
                 variant="outlined"
                 :rules="urlValueRules()"
+                persistent-hint
+                hint="A URL for the protocol"
                 @blur="updateMultiOmicsForm()"
-              >
-                <template #append>
-                  <v-tooltip
-                    right
-                    class="x-2"
-                    max-width="500"
-                  >
-                    <template #activator="{ props }">
-                      <v-icon
-                        v-bind="props"
-                      >
-                        mdi-help-circle
-                      </v-icon>
-                    </template>
-                    <span>
-                      Provide a URL for the protocol. This is a URL for publicly available documentation that describes the experimental protocol in detail, not for the research study publication. Multiple URLs can be provided.
-                    </span>
-                  </v-tooltip>
-                </template>
-              </v-text-field>
+              />
             </v-col>
           </v-row>
-          <div
-            class="v-label theme--light my-2 mx-8 d-flex align-center"
-          >
-            If a published protocol is unavailable, enter the sample preparation protocol(s) here.
+          <div class="mb-2">
+            <b>If a published protocol is unavailable</b>, enter the data acquisition protocol here.
           </div>
-          <v-row
-            class="mx-8"
-            no-gutters
-          >
-            <v-col
-              cols="5"
-            >
-              <v-text-field
-                v-model="currentProtocol.sampleProtocol.name"
-                label="Protocol Name"
-                variant="outlined"
-                @blur="updateMultiOmicsForm()"
-              >
-                <template #append>
-                  <v-tooltip
-                    right
-                    max-width="500"
-                  >
-                    <template #activator="{ props }">
-                      <v-icon
-                        v-bind="props"
-                      >
-                        mdi-help-circle
-                      </v-icon>
-                    </template>
-                    <span>
-                      Provide a name for this protocol. If this protocol is used for other data types, this name will be
-                      used as reference.
-                    </span>
-                  </v-tooltip>
-                </template>
-              </v-text-field>
-            </v-col>
-          </v-row>
-          <v-row
-            class="mx-8"
-            no-gutters
-          >
-            <v-textarea
-              v-model="currentProtocol.sampleProtocol.description"
-              label="Protocol Description"
-              variant="outlined"
-              rows="3"
-              @blur="updateMultiOmicsForm()"
-            />
-          </v-row>
+          <v-text-field
+            v-model="currentProtocol.sampleProtocol.name"
+            label="Protocol Name"
+            class="mb-4"
+            variant="outlined"
+            hint="If this protocol is used for other data types, this name will be used as reference."
+            persistent-hint
+            @blur="updateMultiOmicsForm()"
+          />
+          <v-textarea
+            v-model="currentProtocol.sampleProtocol.description"
+            label="Protocol Description"
+            variant="outlined"
+            rows="3"
+            @blur="updateMultiOmicsForm()"
+          />
         </template>
       </v-expansion-panel-text>
     </v-expansion-panel>
+
     <v-expansion-panel title="Data Acquisition Protocol">
       <v-expansion-panel-text class="text-body-2 px-8">
         <div class="mb-4">
@@ -358,99 +231,32 @@ export default defineComponent({
         />
       </v-expansion-panel-text>
     </v-expansion-panel>
-    <v-expansion-panel>
-      <v-expansion-panel-title>
-        <div>
-          Data Access
-          <v-tooltip
-            right
-            class="x-2"
-            max-width="500"
-          >
-            <template #activator="{ props }">
-              <v-icon
-                v-bind="props"
-                class="ml-1 mb-1"
-                small
-              >
-                mdi-help-circle
-              </v-icon>
-            </template>
-            <span>
-              Provide the location of the publicly available data. This can be a direct URL or a DOI.
-            </span>
-          </v-tooltip>
+
+    <v-expansion-panel title="Data Access">
+      <v-expansion-panel-text class="text-body-2 px-8">
+        <div class="mb-2">
+          Use these fields to provide the location of the publicly available data. The DOI or URL should link to raw instrument data in a public repository.
         </div>
-      </v-expansion-panel-title>
-      <v-expansion-panel-text>
-        <div>
-          <v-row
-            class="mx-8 "
-            no-gutters
-          >
-            <v-col
-              cols="5"
-            >
-              <v-text-field
-                v-model="currentProtocol.dataProtocol.doi"
-                label="DOI"
-                variant="outlined"
-                :rules="doiValueRules()"
-                @blur="updateMultiOmicsForm()"
-              >
-                <template #append>
-                  <v-tooltip
-                    right
-                    class="x-2"
-                    max-width="500"
-                  >
-                    <template #activator="{ props }">
-                      <v-icon
-                        v-bind="props"
-                      >
-                        mdi-help-circle
-                      </v-icon>
-                    </template>
-                    <span>
-                      Provide a DOI to raw instrument data.
-                    </span>
-                  </v-tooltip>
-                </template>
-              </v-text-field>
-            </v-col>
-            <v-col
-              cols="5"
-              class="ml-4"
-            >
-              <v-text-field
-                v-model="currentProtocol.dataProtocol.url"
-                label="URL"
-                variant="outlined"
-                :rules="urlValueRules()"
-                @blur="updateMultiOmicsForm()"
-              >
-                <template #append>
-                  <v-tooltip
-                    right
-                    class="x-2"
-                    max-width="500"
-                  >
-                    <template #activator="{ props }">
-                      <v-icon
-                        v-bind="props"
-                      >
-                        mdi-help-circle
-                      </v-icon>
-                    </template>
-                    <span>
-                      Provide a URL to raw instrument data.
-                    </span>
-                  </v-tooltip>
-                </template>
-              </v-text-field>
-            </v-col>
-          </v-row>
-        </div>
+        <v-row class="mb-4">
+          <v-col>
+            <v-text-field
+              v-model="currentProtocol.dataProtocol.doi"
+              label="DOI"
+              variant="outlined"
+              :rules="doiValueRules()"
+              @blur="updateMultiOmicsForm()"
+            />
+          </v-col>
+          <v-col>
+            <v-text-field
+              v-model="currentProtocol.dataProtocol.url"
+              label="URL"
+              variant="outlined"
+              :rules="urlValueRules()"
+              @blur="updateMultiOmicsForm()"
+            />
+          </v-col>
+        </v-row>
       </v-expansion-panel-text>
     </v-expansion-panel>
   </v-expansion-panels>
