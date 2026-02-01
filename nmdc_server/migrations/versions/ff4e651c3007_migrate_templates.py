f17dc02e145de14b754703972523c75ba156a1cf
eecavanna <eecavanna@users.noreply.github.com>
2026-01-31 16:12:05 -0800

    Configure deprecated `sqlmypy` plugin to ignore 5 lines of code

--- nmdc_server/migrations/versions/ff4e651c3007_migrate_templates.py
+++ nmdc_server/migrations/versions/ff4e651c3007_migrate_templates.py
@@ -30,7 +30,7 @@ def upgrade():
 
     connection = op.get_bind()
     submissions = connection.execute(
-        sa.select(submission_metadata.c.id, submission_metadata.c.metadata_submission)
+        sa.select(submission_metadata.c.id, submission_metadata.c.metadata_submission)  # type: ignore[arg-type]
     )
 
     for submission in submissions:
