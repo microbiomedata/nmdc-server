5c91c0b4cf66b7e2040663bf659dfb17db6348b6
Cristina Stone Pedraza <cristina.stonepedraza@pnnl.gov>
2026-02-05 10:47:53 -0800

    Revert back to <script setup> style

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,74 +1,60 @@
-<script lang="ts">
-import {
-  defineComponent,
-  onMounted,
-  watch,
-  ref,
-} from 'vue';
-import AppHeader from '@/components/Presentation/AppHeader.vue';
-import UserEmailModal from './components/UserEmailModal.vue';
-import { stateRefs, init } from '@/store/';
+<script lang="ts" setup>
+import { onMounted, onUnmounted, watch, ref, defineComponent } from 'vue';
 import { useRouter } from 'vue-router';
 import { api, REFRESH_TOKEN_EXPIRED_EVENT, RefreshTokenExchangeError } from '@/data/api';
+import { init, stateRefs } from '@/store/';
 
-export default defineComponent({
-  name: 'App',
-  components: { AppHeader, UserEmailModal },
-  setup() {
-    const router = useRouter();
+const router = useRouter();
 
-    const handleRefreshTokenExpired = () => {
-      stateRefs.user.value = null;
-      if (router) {
-        init(router, false);
-      }
-    };
+const handleRefreshTokenExpired = () => {
+  stateRefs.user.value = null;
+  if (router) {
+    init(router, false);
+  }
+};
 
-    const { user } = stateRefs;
+const { user } = stateRefs;
 
-    const showEmailModal = ref(false);
+const showEmailModal = ref(false);
 
-    watch(
-      user,
-      (newUser) => {
-        const missingEmail = !!newUser && !newUser.email?.trim();
-        showEmailModal.value = missingEmail;
-      },
-      { immediate: true },
-    );
-
-    onMounted(async () => {
-      window.addEventListener(REFRESH_TOKEN_EXPIRED_EVENT, handleRefreshTokenExpired);
-
-      if (!router) {
-        return;
-      }
-      if (router.currentRoute.value.path === '/login') {
-        // init() will be called in the LoginPage component in this case
-        return;
-      }
-      // The first time the app is loaded, there will not yet be an access token configured on
-      // the API client. There *may* be a refresh token in storage, so attempt to exchange it
-      // before calling the init() function. The exchangeRefreshToken function will throw a
-      // RefreshTokenExchangeError if the refresh token is invalid or expired. If that or any other
-      // exception is thrown during the exchange, we still want to call init() to finish setting up
-      // the app, but we don't want to re-throw the error.
-      try {
-        await api.exchangeRefreshToken();
-      } catch (error) {
-        if (!(error instanceof RefreshTokenExchangeError)) {
-          throw error;
-        }
-      } finally {
-        await init(router);
-      }
-    });
-
-    return {
-      stateRefs,
-      showEmailModal,
-    };
+watch(
+  user,
+  (newUser) => {
+    const missingEmail = !!newUser && !newUser.email?.trim();
+    showEmailModal.value = missingEmail;
   },
+  { immediate: true },
+);
+
+onMounted(async () => {
+  window.addEventListener(REFRESH_TOKEN_EXPIRED_EVENT, handleRefreshTokenExpired);
+
+  if (!router) {
+    return;
+  }
+  if (router.currentRoute.value.path === '/login') {
+    // init() will be called in the LoginPage component in this case
+    return;
+  }
+  // The first time the app is loaded, there will not yet be an access token configured on
+  // the API client. There *may* be a refresh token in storage, so attempt to exchange it
+  // before calling the init() function. The exchangeRefreshToken function will throw a
+  // RefreshTokenExchangeError if the refresh token is invalid or expired. If that or any other
+  // exception is thrown during the exchange, we still want to call init() to finish setting up
+  // the app, but we don't want to re-throw the error.
+  try {
+    await api.exchangeRefreshToken();
+  } catch (error) {
+    if (!(error instanceof RefreshTokenExchangeError)) {
+      throw error;
+    }
+  } finally {
+    await init(router);
+  }
+});
+
+onUnmounted(() => {
+  window.removeEventListener(REFRESH_TOKEN_EXPIRED_EVENT, handleRefreshTokenExpired);
 });
 </script>
 
@@ -79,4 +65,3 @@ export default defineComponent({
     <user-email-modal v-model:value="showEmailModal" />
   </v-layout>
 </template>
- 
