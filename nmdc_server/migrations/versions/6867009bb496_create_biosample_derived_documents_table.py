cb420493451b1487d01c9f6aa2ff1967558f26f5
eecavanna <eecavanna@users.noreply.github.com>
2026-02-26 02:14:43 -0800

    Reorder imports to appease linter

--- nmdc_server/migrations/versions/6867009bb496_create_biosample_derived_documents_table.py
+++ nmdc_server/migrations/versions/6867009bb496_create_biosample_derived_documents_table.py
@@ -8,8 +8,8 @@ Create Date: 2026-02-26 08:56:09.654967
 
 from typing import Optional
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
