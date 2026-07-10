a75fd90abaebc33337560577ee54894fad348749
eecavanna <eecavanna@users.noreply.github.com>
2026-07-09 18:14:58 -0700

    Update table body to handle INSDC identifier strings (not just arrays)

--- web/src/components/AmpliconObjectDataTable.vue
+++ web/src/components/AmpliconObjectDataTable.vue
@@ -25,7 +25,7 @@ export default defineComponent({
       { title: 'Target Subfragment', value: 'target_subfragment' },
     ];
 
-    // Derive the table rows from the `omicsProcessing` props (an array).
+    // Derive the table rows from the `omicsProcessing` prop (an array).
     const items = computed(() => props.omicsProcessing.map((op) => ({
       type: op.annotations.type,
       insdc_experiment_identifiers: op.annotations.insdc_experiment_identifiers,
@@ -33,10 +33,29 @@ export default defineComponent({
       target_subfragment: op.annotations.target_subfragment,
     })));
 
+    /**
+     * Returns the first INSDC Experiment Identifier represented by the specified item (of the kind
+     * of "items" that we derive from the `omicsProcessing` prop).
+     * 
+     * Note: We use this helper function because the relevant field can contain either a string or
+     *       an array of strings and we don't want to clutter the template with that logic.
+     *       "Why do we give special consideration to the first one?" I don't know. This is a
+     *       refactor of existing code, which did not include anyone's rationale for doing that.
+     */
+    const getFirstInsdcExperimentIdentifier = (item: {insdc_experiment_identifiers?: OmicsProcessingResult["annotations"]["insdc_experiment_identifiers"]}): string | null => {
+      const value = item.insdc_experiment_identifiers;
+      if (Array.isArray(value)) {
+        return typeof value[0] === "string" ? value[0] : null;
+      } else {
+        return typeof value === "string" ? value : null;
+      }
+    }
+
     return {
       headers,
       items,
       BioprojectLinkBase,
+      getFirstInsdcExperimentIdentifier,
     };
   },
 });
@@ -50,11 +69,11 @@ export default defineComponent({
     <!-- eslint-disable-next-line -->
     <template #item.insdc_experiment_identifiers="{ item }">
       <a
-        :href="BioprojectLinkBase + item?.insdc_experiment_identifiers"
+        :href="BioprojectLinkBase + getFirstInsdcExperimentIdentifier(item)"
         target="_blank"
         rel="noopener noreferrer"
       >
-        {{ item && item.insdc_experiment_identifiers && item.insdc_experiment_identifiers[0] }}
+        {{ item && getFirstInsdcExperimentIdentifier(item) }}
       </a>
     </template>
   </v-data-table>
