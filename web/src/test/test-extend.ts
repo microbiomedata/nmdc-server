679c87a5857eaaafe012c3becd513e8ecf146961
JamesTessmer <james.tessmer@gmail.com>
2026-08-06 16:29:06 -0700

    Fix linting

--- web/src/test/test-extend.ts
+++ web/src/test/test-extend.ts
@@ -3,15 +3,17 @@ import { createPinia, setActivePinia } from 'pinia';
 
 export const test = testBase.extend({
   pinia: [
-    async ({}, use) => {
-      // Create a fresh pinia instance for each test
-      const pinia = createPinia();
-      setActivePinia(pinia);
-      
-      await use(pinia);
-      
-      // Cleanup after test
-      setActivePinia(undefined as any);
+    async ({ task }, use) => {
+      if ( task !== null) {
+        // Create a fresh pinia instance for each test
+        const pinia = createPinia();
+        setActivePinia(pinia);
+        
+        await use(pinia);
+        
+        // Cleanup after test
+        setActivePinia(undefined as any);
+      }
     },
     {
       auto: true,
