33ed5103a6a56f636b9c0cfd93c98258139d9001
eecavanna <134325062+eecavanna@users.noreply.github.com>
2026-08-07 14:48:56 -0700

    Clarify variable name and make type hints more specific

--- nmdc_server/ingest/study.py
+++ nmdc_server/ingest/study.py
@@ -19,9 +19,12 @@ logger = get_logger(__name__)
 # Define how long we want `requests` to wait (a) to establish a connection to the remote server,
 # and (b) for the remote server to send the first (or any subsequent) byte of the response.
 # Docs: https://docs.python-requests.org/en/latest/user/advanced/#timeouts
-requests_timeout_for_connection = 5  # in seconds
-requests_timeout_between_response_bytes = 20  # in seconds
-requests_timeout: tuple = (requests_timeout_for_connection, requests_timeout_between_response_bytes)
+requests_timeout_for_connection: float = 5.0  # in seconds
+requests_timeout_for_next_byte_of_response: float = 20.0  # in seconds
+requests_timeout: tuple[float, float] = (
+    requests_timeout_for_connection,
+    requests_timeout_for_next_byte_of_response
+)
 
 
 def get_or_create_pi(db: Session, name: str, url: Optional[str], orcid: Optional[str]) -> str:
