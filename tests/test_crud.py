d5b5738722b44158c4ba5fbca73c1dfbe01e5b9f
Cody O'Donnell <ctodonnell@lbl.gov>
2026-08-10 14:24:04 -0700

    Add test for zip path creation

---
+++ tests/test_crud.py
@@ -0,0 +1,24 @@
+from nmdc_server import crud, models
+
+
+def test_construct_zip_file_path_uses_data_generation_and_workflow_names():
+    data_generation = models.OmicsProcessing(
+        id="nmdc:dgns-1",
+        name="Data/Generation",
+    )
+    data_object = models.DataObject(
+        id="nmdc:dobj-1",
+        name="reads.fastq.gz",
+    )
+    workflow_activity = models.ReadsQC(
+        id="nmdc:wfrqc-1",
+        name="Reads:QC",
+    )
+    data_generation.outputs.append(data_object)
+    data_generation.reads_qc.append(workflow_activity)
+    workflow_activity.outputs.append(data_object)
+
+    assert (
+        crud.construct_zip_file_path(data_object)
+        == "Data_Generation/Reads_QC/reads.fastq.gz"
+    )
