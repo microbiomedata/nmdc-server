74ab3f504d48cc8833d63b297a2df352d5a989c0
olivia.hess <olivia.hess@pnnl.gov>
2026-09-09 10:45:52 -0400

    fix loading bar

--- web/src/use/useRequest.ts
+++ web/src/use/useRequest.ts
@@ -10,23 +10,28 @@ export default function useRequest() {
     count: 0, // indicates number of successful calls
   });
 
+  let activeRequests = 0;
+
    /**
     * Helper function to wrap an async request and manage loading and error state.
     * Callers of this function can check state.error to determine if an error occurred.
     * If state.error is null, then an error has not (yet) occurred.
-    * @param func 
-    * @returns 
+    * @param func
+    * @returns
     */
   async function request<T>(func: () => Promise<T>) {
     try {
+      activeRequests += 1;
       state.loading = true;
       state.error = null;
       const val = await func();
       state.count += 1;
-      state.loading = false;
+      activeRequests -= 1;
+      if (activeRequests === 0) state.loading = false;
       return val;
     } catch (err) {
-      state.loading = false;
+      activeRequests -= 1;
+      if (activeRequests === 0) state.loading = false;
       state.error = String(err);
       return null;
     }
